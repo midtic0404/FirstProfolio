45324d70596678badd548630c8fdd58f44fcc886
Chia Che Chang <midtic0404@gmail.com>
2017-12-11 20:12:34 -0700

    Making Sticky Nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
     
-    
+    //for way point sticky nav
+    $('.js--section-bio').waypoint(function(direction){
+       if(direction == "down"){
+           $('nav').addClass('sticky');
+       }else{
+           $('nav').removeClass('sticky');
+       } 
+    },{
+        offset: '60px'
+    });
     
     
     //For contact map
